779002c7f65e96b2d4b9a1a68e700057cb275490
Markus Klink Damgaard <markus.klink.damgaard@gmail.com>
2025-05-26 22:17:25 +0200

    Add collision

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,22 @@ struct Shape {
     y: f32,
 }
 
+impl Shape {
+    fn collides_with(&self, other: &Self) -> bool {
+        self.rect().overlaps(&other.rect())
+        
+    }
+    fn rect(&self) -> Rect {
+        Rect {
+        x: self.x - self.size / 2.0,
+        y: self.y - self.size / 2.0,
+        w: self.size,
+        h: self.size,
+        }
+    }
+}
+    
+
 fn move_circle(circle: Shape, delta_time: f32) -> Shape {
     let mut new_circle = Shape {
         size: circle.size,
@@ -46,6 +62,7 @@ fn draw_squares(squares: &[Shape]) {
 
 #[macroquad::main("My game!")]
 async fn main() {
+    let mut gameover = false;
     rand::srand(miniquad::date::now() as u64);
     let mut squares: Vec<Shape> = vec![];
     const MOVEMENT_SPEED: f32 = 200.0;
@@ -56,26 +73,52 @@ async fn main() {
         y: screen_height() / 2.0,
     };
     loop {
-        if rand::gen_range(0, 99) >= 95 {
-            let size = rand::gen_range(16.0, 64.0);
-            squares.push(Shape {
-                size,
-                speed: rand::gen_range(50.0, 150.0),
-                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
-                y: -size,
-            });
-        }
-        let delta_time = get_frame_time();
         clear_background(SKYBLUE);
-
-        circle = move_circle(circle, delta_time);
-        draw_circle(circle.x, circle.y, circle.size, DARKBLUE);
-
-        for square in &mut squares {
-            square.y += square.speed * delta_time;
-        }            
+        let delta_time = get_frame_time();
+        if !gameover {
+            if rand::gen_range(0, 99) >= 95 {
+                let size = rand::gen_range(16.0, 64.0);
+                squares.push(Shape {
+                    size,
+                    speed: rand::gen_range(50.0, 150.0),
+                    x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                    y: -size,
+                });
+            }
+    
+            circle = move_circle(circle, delta_time);
+            for square in &mut squares {
+                square.y += square.speed * delta_time;
+            } 
+        }
+        
+        draw_circle(circle.x, circle.y, circle.size, DARKBLUE);           
         squares.retain(|square| square.y < screen_height() + square.size);
         draw_squares(&squares);
+        
+        if squares.iter().any(|square| circle.collides_with(square)) {
+            gameover = true;
+        }
+        
+        if gameover {
+            let text = "GAME OVER!";
+            let text_dimensions = measure_text(text, None, 50, 1.0);
+            draw_text(
+                text,
+                screen_width() / 2.0 - text_dimensions.width / 2.0,
+                screen_height() / 2.0,
+                50.0,
+                RED,
+            );
+        }                
+
+        if gameover && is_key_pressed(KeyCode::Space) {
+            squares.clear();
+            circle.x = screen_width() / 2.0;
+            circle.y = screen_height() / 2.0;
+            gameover = false;
+        }
+            
 
         next_frame().await
     }
