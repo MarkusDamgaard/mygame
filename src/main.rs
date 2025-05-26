2b70ef4da27a4d0a6c2a18c4ff31be9161b94edc
Markus Klink Damgaard <markus.klink.damgaard@gmail.com>
2025-05-26 20:53:35 +0200

    change colors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("My game!")]
 async fn main() {
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(GREEN);
         next_frame().await
     }
 }
